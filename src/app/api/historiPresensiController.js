f4ffe631442b675f0275452dee72ccd0532a525b
Sutisna <sutisna@sawala.co>
2022-11-05 16:27:46 +0700

    add: histori presensi by date

--- src/app/api/historiPresensiController.js
+++ src/app/api/historiPresensiController.js
@@ -69,6 +69,65 @@ module.exports = {
       res.send(listAbsensi);
     }
   },
+  getDateParam: async (req, res) => {
+    // if (!(await req.user.hasAccess(_module, "create"))) {
+    //   return error(res).permissionError();
+    // }
+
+    const validation = validationResult(req);
+    if (!validation.isEmpty()) {
+      return error(res).validationError(validation.array());
+    }
+
+    const mAbsensi = await Absensi.findAll({
+      where: {
+        kode_pegawai: req.query.kode_pegawai,
+        [Op.and]: [
+          Sequelize.where(
+            Sequelize.fn("date", Sequelize.col("timestamp_absensi")),
+            "=",
+            moment(req.query.tanggal).format("YYYY-MM-DD")
+          ),
+        ],
+      },
+    });
+
+    if (mAbsensi) {
+      let listAbsensi = [];
+      const monthName = [
+        "Jan",
+        "Feb",
+        "Mar",
+        "Apr",
+        "Mei",
+        "Jun",
+        "Jul",
+        "Agu",
+        "Sep",
+        "Okt",
+        "Nov",
+        "Des",
+      ];
+      mAbsensi.forEach((item, index) => {
+        let dateFormat = date.format(item.timestamp_absensi, "DD M YYYY HH:mm");
+        let splitDate = dateFormat.split(" ");
+        const getMonthName = monthName[parseInt(splitDate[1]) - 1];
+        splitDate[1] = getMonthName;
+        listAbsensi.push({
+          timestamp_absensi: splitDate.join(" "),
+          tipe_absensi: item.tipe_absensi,
+          label_absensi: item.label_absensi,
+          time_limit_datang: item.time_limit_datang,
+          time_limit_pulang: item.time_limit_pulang,
+          catatan_absensi:
+            item.catatan_absensi.length > 30
+              ? item.catatan_absensi.substring(0, 30) + "..."
+              : item.catatan_absensi,
+        });
+      });
+      res.send(listAbsensi);
+    }
+  },
   // Validation
   validate: (type) => {
     let mAbsensi = null;
