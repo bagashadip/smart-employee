87a41963859f3b3972b654684be9afd34d4f8ed1
Sutisna <sutisna@sawala.co>
2022-12-24 11:44:05 +0100

    add: get histori presensi yesterday

--- src/app/api/historiPresensiController.js
+++ src/app/api/historiPresensiController.js
@@ -132,6 +132,151 @@ module.exports = {
       res.send(listAbsensi);
     }
   },
+  getYesterday: async (req, res) => {
+    if (!(await req.user.hasAccess(_module, "view"))) {
+      return error(res).permissionError();
+    }
+
+    const validation = validationResult(req);
+    if (!validation.isEmpty()) {
+      return error(res).validationError(validation.array());
+    }
+
+    let limitDatang, limitPulang, mAbsensi, mAbsensiToday;
+
+    //Absensi yesterday
+    mAbsensi = await Absensi.findAll({
+      where: {
+        kode_pegawai: req.query.kode_pegawai,
+        [Op.and]: [
+          Sequelize.where(
+            Sequelize.fn("date", Sequelize.col("timestamp_absensi")),
+            "=",
+            moment().subtract(1, 'days').format('YYYY-MM-DD')
+          ),
+        ],
+      },
+    });
+
+    //Absensi of yesterday found
+    if(mAbsensi)
+    {
+      mAbsensi.forEach((item, index) => {
+        limitDatang = item.time_limit_datang;
+        limitPulang = item.time_limit_pulang;
+      });
+    }
+
+    //Get today's absensi
+    mAbsensiToday = await Absensi.findAll({
+      where: {
+        kode_pegawai: req.query.kode_pegawai,
+        [Op.and]: [
+          Sequelize.where(
+            Sequelize.fn("date", Sequelize.col("timestamp_absensi")),
+            "=",
+            moment().format('YYYY-MM-DD')
+          ),
+        ],
+      },
+    });
+
+    //Absensi of yesterday not found
+    if(!mAbsensi)
+    {
+      if(mAbsensiToday)
+      {
+        mAbsensi.forEach((item, index) => {
+          limitDatang = item.time_limit_datang;
+          limitPulang = item.time_limit_pulang;
+        });
+      }else{
+        res.send([]);
+      }
+    }
+
+    console.log(limitDatang > limitPulang)
+
+    const monthName = [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "Mei",
+      "Jun",
+      "Jul",
+      "Agu",
+      "Sep",
+      "Okt",
+      "Nov",
+      "Des",
+    ];
+
+    if(limitDatang > limitPulang)
+    {
+
+      //Get last 12 hours of today's absensi
+      const mAbsensiLast12Hours = await Absensi.findAll({
+        where: {
+          kode_pegawai: req.query.kode_pegawai,
+          [Op.and]: [
+            Sequelize.where(
+              Sequelize.fn("date", Sequelize.col("timestamp_absensi")),
+              ">=",
+              moment().subtract(12, 'hours').format('YYYY-MM-DD HH:mm:ss')
+            ),
+          ],
+        },
+      });
+
+      if(mAbsensiLast12Hours){
+        let listAbsensi = [];
+        mAbsensiLast12Hours.forEach((item, index) => {
+          let dateFormat = date.format(item.timestamp_absensi, "DD M YYYY HH:mm");
+          let splitDate = dateFormat.split(" ");
+          const getMonthName = monthName[parseInt(splitDate[1]) - 1];
+          splitDate[1] = getMonthName;
+          listAbsensi.push({
+            timestamp_absensi: splitDate.join(" "),
+            tipe_absensi: item.tipe_absensi,
+            label_absensi: item.label_absensi,
+            time_limit_datang: item.time_limit_datang,
+            time_limit_pulang: item.time_limit_pulang,
+            catatan_absensi:
+              item.catatan_absensi.length > 30
+                ? item.catatan_absensi.substring(0, 30) + "..."
+                : item.catatan_absensi,
+          });
+        });
+        res.send(listAbsensi);
+      }
+    }else if(mAbsensiToday){
+      let listAbsensi = [];
+      
+      mAbsensiToday.forEach((item, index) => {
+        let dateFormat = date.format(item.timestamp_absensi, "DD M YYYY HH:mm");
+        let splitDate = dateFormat.split(" ");
+        const getMonthName = monthName[parseInt(splitDate[1]) - 1];
+        splitDate[1] = getMonthName;
+        listAbsensi.push({
+          timestamp_absensi: splitDate.join(" "),
+          tipe_absensi: item.tipe_absensi,
+          label_absensi: item.label_absensi,
+          time_limit_datang: item.time_limit_datang,
+          time_limit_pulang: item.time_limit_pulang,
+          catatan_absensi:
+            item.catatan_absensi.length > 30
+              ? item.catatan_absensi.substring(0, 30) + "..."
+              : item.catatan_absensi,
+        });
+      });
+      res.send(listAbsensi);
+    }else{
+      res.send([]);
+    }
+
+    
+  },
   // Validation
   validate: (type) => {
     let mAbsensi = null;
