095d877e0f4aa6549861d39c1919f5455115d484
Sutisna <sutisna@sawala.co>
2022-12-30 11:45:59 +0100

    add: new migrate to recreate kegiatan

---
+++ src/migrations/20221230104505-recreate_kegiatan.js
@@ -0,0 +1,45 @@
+"use strict";
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable("tbl_kegiatan", {
+      id_kegiatan: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID
+      },
+      foto_kegiatan: {
+        type: Sequelize.UUID
+      },
+      desc_kegiatan: {
+        type: Sequelize.TEXT
+      },
+      nama_kegiatan: {
+        type: Sequelize.TEXT
+      },
+      tanggal_kegiatan: {
+        type: Sequelize.DATEONLY
+      },
+      kode_pegawai: {
+        type: Sequelize.STRING(30),
+        references: {
+          model: "tbl_pegawai",
+          key: "kode_pegawai",
+        },
+        onUpdate: "CASCADE",
+        onDelete: "SET NULL",
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+      },
+      waktu_kegiatan_mulai : {
+        type: Sequelize.TIME
+      },
+      waktu_kegiatan_selesai : {
+        type: Sequelize.TIME
+      },
+    });
+  }
+};
