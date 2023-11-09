7ac4389270e2807a7a3c6a7bf3ef644a5342f694
Hendry Priyatno <hendry.priyatno@gmail.com>
2023-11-09 08:42:18 +0700

    Create eventController.js

---
+++ src/app/event/eventController.js
@@ -0,0 +1,14 @@
+const _module = "event";
+const _ = require("lodash");
+const { body, query, validationResult } = require("express-validator");
+const Sequelize = require("sequelize");
+const error = require("../../util/errors");
+const datatable = require("../../util/datatable");
+const moment = require("moment");
+const Op = Sequelize.Op;
+
+
+
+module.exports = {
+    
+}
